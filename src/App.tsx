80c5f6806c16b1aa6b8e1bf9db53fc2969bb744b
Lukasz Matuszewski <lucas.matuszewski@gmail.com>
2020-08-25 01:30:58 +0200

    Add Highcharts with sample simple chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
 import './App.css';
 
-function App() {
+const options: Highcharts.Options = {
+  title: {
+    text: 'My chart',
+  },
+  series: [
+    {
+      type: 'line',
+      data: [1, 2, 3],
+    },
+  ],
+};
+
+const App = (props: HighchartsReact.Props) => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Dashboard with Highcharts</h1>
+        <HighchartsReact highcharts={Highcharts} options={options} {...props} />
       </header>
     </div>
   );
-}
+};
 
 export default App;
